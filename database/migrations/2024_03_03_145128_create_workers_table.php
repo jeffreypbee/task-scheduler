6e98ca7b388e405cc6eb3007a4cd0ab03fae3bc8
Jeffrey Bee <jeffreypbee@gmail.com>
2024-03-03 09:54:40 -0500

    Update 2024_03_03_145128_create_workers_table.php

--- database/migrations/2024_03_03_145128_create_workers_table.php
+++ database/migrations/2024_03_03_145128_create_workers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('workers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('color');
             $table->timestamps();
         });
     }
